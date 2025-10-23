4902419d2a0f7f23fb264efd38b525c3183344d9
migueljmFP <miguel.jimenezmorales.25@campusfp.es>
2025-10-24 00:34:32 +0200

    Delete p.sumanumeros.py

--- p.sumanumeros.py
+++
@@ -1,8 +0,0 @@
-contador = 0
-numero = int(input("Introduce un número:"))
-
-while numero > -1:
-	contador += numero
-	numero = int(input("Introduce otro número:"))
-
-print("La suma total de los números es:", contador)
