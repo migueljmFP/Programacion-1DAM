40f568277dcc968913a0ce5b917835a7f3b0be2d
migueljmFP <miguel.jimenezmorales.25@campusfp.es>
2025-10-25 13:26:19 +0200

    Actualizar minitiendaonline.py
    Actualizarlo para el ejercicio 1 de 3

--- minitiendaonline.py
+++ minitiendaonline.py
@@ -20,7 +20,7 @@ def mostrar_menu(n):
 
 def crear_articulo(n):
     nombre = input("Introduce el nombre del artículo: ")
-    if nombre in articulos:
+    if nombre in n:
         print("¡El nombre ya está en la lista de artículos!")
     else:
         precio = float(input("Introduce un precio para el artículo (con decimales): "))
@@ -31,31 +31,83 @@ def crear_articulo(n):
             if stock <= 0:
                 print("El stock debe ser entero (mayor que 0, y sin decimales)")
             else:
-                id = int(input("Introduce un id al producto"))
-                if id in articulos:
+                id = int(input("Introduce un id al producto: "))
+                if id in n:
                     print("El id del artículo no se puede repetir")
                 else:
-                    articulos[articulo].append(id, nombre, precio, stock, "True")
+                    articulo.update({"id": id, "nombre": nombre,"precio": precio,"stock": stock,"activo": True})
+                    n.append(articulo.copy())
+                    print(f"El artículo '{nombre}' fue añadido correctamente")
+
+def listar_articulos(n):
+    if not n:
+        print("No hay artículos para mostrar")
+    for i, articulo in enumerate(n, start=1):
+        print(f"{i}. {articulo['nombre']} - ID: {articulo['id']}, Precio: {articulo['precio']}, Stock: {articulo['stock']}, ¿Está activo?: {articulo['activo']}")
+
+def buscar_por_id(n):
+    id_buscar = int(input("Introduce el id del artículo que quieres buscar: "))
+    for articulo in n:
+        if articulo['id'] == id_buscar:
+            print(f"Artículo encontrado: {articulo}")
+            return
+    print("Artículo no encontrado")
+
+def actualizar_articulo(n):
+    actualizar = int(input("Introduce el id del artículo que quieras actualizar: "))
+    for articulo in n:
+        if articulo['id'] == actualizar:
+            nombre_nuevo = input("Introduce un nuevo nombre: ") or articulo['nombre']
+            precio_nuevo = float(input("Introduce un nuevo precio (con decimales): ")) or articulo['precio']
+            stock_nuevo = int(input("Introduce una nueva cantidad: ")) or articulo['stock']
+
+            articulo['nombre'] = nombre_nuevo
+            articulo['precio'] = precio_nuevo
+            articulo['stock'] = stock_nuevo
+
+            print("Artículo actualizado correctamente")
+            return
+        
+    print("Artículo no encontrado")
+
+def eliminar_articulo(n):
+    eliminar = input("Introduce el nombre del artículo que quieres eliminar: ")
+    for i in n:
+        if articulo['nombre'] == eliminar:
+            articulos.remove(articulo)
+            print("El artículo ha sido eliminado correctamente")
+            return
+    print("Artículo no encontrado")
+
+def cambiar_activo(n):
+    cambiar = input("Introduce el nombre del artículo que quieras alternar entre activo e inactivo: ")
+    for articulo in n:
+        if articulo['nombre'] == cambiar:
+            if articulo['activo'] == False:
+                articulo['activo'] = True
+            else:
+                articulo['activo'] = False
+            print("Se ha actualizado el estado activo/inactivo")
+    print("Artículo no encontrado")
 
 def ejecutar_opcion(opcion):
         match opcion:
             case 1:
-                crear_articulo(opcion)
+                crear_articulo(articulos)
             case 2:
-                print("hola mundo")
+                listar_articulos(articulos)
             case 3:
-                print("hola mundo")
+                buscar_por_id(articulos)
             case 4:
-                print("hola mundo")
+                actualizar_articulo(articulos)
             case 5:
-                print("hola mundo")
+                eliminar_articulo(articulos)
             case 6:
-                print("hola mundo")
+                cambiar_activo(articulos)
             case 7:
                 print("Saliendo del programa... ¡Hasta pronto!")
             case _:
                 print("Número no valido")
-
 
 #definición de lógica de programación
 opcion = 0
